d0d992a48b5b5fe8008fdae2657422838446d5ff
“shamnad” <hamzashamnad123@gmail.com>
2023-12-02 00:48:09 +0530

    lc2032

--- test.py
+++ test.py
@@ -1,33 +1,16 @@
-# # lc2032
-# class Solution:
-#     def twoOutOfThree(self, nums1, nums2, nums3):
-#         com=[]
-#         for i in range(len(nums1)):
-
-#             for j in range(len(nums2)):
-
-#                 for k in range(len(nums3)):
-                    
-#                     if nums1[i]==nums2[j]:
-#                         com.append(nums1[i])
-
-#                     elif nums1[i]==nums3[k]:
-#                         com.append(nums2[j])
-
-#                     elif nums2[j]==nums3[k]:
-#                         com.append(nums2[j])
-
-#                     else:
-#                         return []
-#         return com
-
-# obj=Solution()
-# nums1 = [1,1,3,2]
-# nums2 = [2,3]
-# nums3 = [3]
-# a=obj.twoOutOfThree(nums1,nums2,nums3)
-# print(a)
-
-# gjit gi
-
-test case 2 
+# lc2032 correct answer
+class Solution:
+    def twoOutOfThree(self, nums1, nums2, nums3):
+        com=[]
+        for i in nums1+nums2+nums3:
+            if i in nums1 and i in nums2 or i in nums2 and i in nums3 or i in nums1 and i in nums3:
+                com.append(i)
+            
+        return set(com)
+    
+obje=Solution()
+nums1 = [1,1,3,2]
+nums2 = [2,3]
+nums3 = [3]
+a=obje.twoOutOfThree(nums1,nums2,nums3)
+print(a)
