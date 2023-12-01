0e4aec8d17c123191001823b148f3b85aee2996e
“shamnad” <hamzashamnad123@gmail.com>
2023-12-02 00:12:11 +0530

    firstpush

--- test.py
+++ test.py
@@ -1,29 +1,29 @@
-# lc2032
-class Solution:
-    def twoOutOfThree(self, nums1, nums2, nums3):
-        com=[]
-        for i in range(len(nums1)):
+# # lc2032
+# class Solution:
+#     def twoOutOfThree(self, nums1, nums2, nums3):
+#         com=[]
+#         for i in range(len(nums1)):
 
-            for j in range(len(nums2)):
+#             for j in range(len(nums2)):
 
-                for k in range(len(nums3)):
+#                 for k in range(len(nums3)):
                     
-                    if nums1[i]==nums2[j]:
-                        com.append(nums1[i])
+#                     if nums1[i]==nums2[j]:
+#                         com.append(nums1[i])
 
-                    elif nums1[i]==nums3[k]:
-                        com.append(nums2[j])
+#                     elif nums1[i]==nums3[k]:
+#                         com.append(nums2[j])
 
-                    elif nums2[j]==nums3[k]:
-                        com.append(nums2[j])
+#                     elif nums2[j]==nums3[k]:
+#                         com.append(nums2[j])
 
-                    else:
-                        return []
-        return com
+#                     else:
+#                         return []
+#         return com
 
-obj=Solution()
-nums1 = [1,1,3,2]
-nums2 = [2,3]
-nums3 = [3]
-a=obj.twoOutOfThree(nums1,nums2,nums3)
-print(a)
+# obj=Solution()
+# nums1 = [1,1,3,2]
+# nums2 = [2,3]
+# nums3 = [3]
+# a=obj.twoOutOfThree(nums1,nums2,nums3)
+# print(a)
