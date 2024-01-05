90ea82bf9f64dc26383a1330943a6275f108cc46
“shamnad” <hamzashamnad123@gmail.com>
2024-01-05 22:07:24 +0530

    lc 2562

--- test.py
+++ test.py
@@ -530,7 +530,7 @@
 
 # 2578. Split With Minimum Sum
 
-Example 1:
+# Example 1:
 
 # Input: num = 4325
 # Output: 59
@@ -541,21 +541,79 @@ Example 1:
 # Output: 75
 # Explanation: We can split 687 so that num1 is 68 and num2 is 7, which would give an optimal sum of 75.
 
-class Solution:
-    def splitNum(self,num):
-        s = str(num)
-        s = sorted(s)
-        a, b = '', ''
-        for i in range(len(s)):
-            if i % 2 == 0:
-                a += s[i]
-            else:
-                b += s[i]
-        return int(a) + int(b)
+# class Solution:
+#     def splitNum(self,num):
+#         s = str(num)
+#         s = sorted(s)
+#         a, b = '', ''
+#         for i in range(len(s)):
+#             if i % 2 == 0:
+#                 a += s[i]
+#             else:
+#                 b += s[i]
+#         return int(a) + int(b)
 
 
     
-ob=Solution()
-num = 4325
-a=ob.splitNum(num)
+# ob=Solution()
+# num = 4325
+# a=ob.splitNum(num)
+# print(a)
+
+
+
+
+
+# 2562. Find the Array Concatenation Value
+# Easy
+# 329
+# 11
+# Companies
+# You are given a 0-indexed integer array nums.
+
+# The concatenation of two numbers is the number formed by concatenating their numerals.
+
+# For example, the concatenation of 15, 49 is 1549.
+# The concatenation value of nums is initially equal to 0. Perform this operation until nums becomes empty:
+
+# If there exists more than one number in nums, pick the first element and last element in nums respectively and add the value of their concatenation to the concatenation value of nums, then delete the first and last element from nums.
+# If one element exists, add its value to the concatenation value of nums, then delete it.
+# Return the concatenation value of the nums.
+
+ 
+
+# Example 1:
+
+# Input: nums = [7,52,2,4]
+# Output: 596
+# Explanation: Before performing any operation, nums is [7,52,2,4] and concatenation value is 0.
+#  - In the first operation:
+# We pick the first element, 7, and the last element, 4.
+# Their concatenation is 74, and we add it to the concatenation value, so it becomes equal to 74.
+# Then we delete them from nums, so nums becomes equal to [52,2].
+#  - In the second operation:
+# We pick the first element, 52, and the last element, 2.
+# Their concatenation is 522, and we add it to the concatenation value, so it becomes equal to 596.
+# Then we delete them from the nums, so nums becomes empty.
+# Since the concatenation value is 596 so the answer is 596.
+
+
+class Solution:
+    def findTheArrayConcVal(self, nums):
+        concat=0
+        
+        while len(nums)>0:
+            if len(nums)>1:
+                concat+= int(str(nums[0])+str(nums[-1]))
+                del nums[-1]
+            else:
+                concat+= nums[0]
+            
+            del nums[0]
+        return  concat
+        
+        
+obj=Solution()
+nums = [7,52,2,4]
+a=obj.findTheArrayConcVal(nums)
 print(a)
