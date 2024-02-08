15fc11aad538043c8905a0abcb6cda456672a485
“shamnad” <hamzashamnad123@gmail.com>
2024-02-09 00:09:22 +0530

    lc 2903

--- test.py
+++ test.py
@@ -1175,29 +1175,113 @@
 # Explanation: Integer "123" has no trailing zeros, we return integer "123".
     
     
-class Solution:
-    def removeTrailingZeros(self,num):
-        j=0
-        nm=int(num)
-        if nm%10==0:
-                rev=num[::-1]
-                for i in rev:
-                        if i =='0':
-                                j+=1
-                        else:
-                              finalres=rev[j:]
-                              res=finalres[::-1]
-                              return res
+# class Solution:
+#     def removeTrailingZeros(self,num):
+#         j=0
+#         nm=int(num)
+#         if nm%10==0:
+#                 rev=num[::-1]
+#                 for i in rev:
+#                         if i =='0':
+#                                 j+=1
+#                         else:
+#                               finalres=rev[j:]
+#                               res=finalres[::-1]
+#                               return res
                       
 
-                print(nm)
-        else:
-              return num
+#                 print(nm)
+#         else:
+#               return num
               
 
-obj=Solution()
-num = "51230100"
-# num = "123"
-a=obj.removeTrailingZeros(num)
-print(a)
+# obj=Solution()
+# num = "51230100"
+# # num = "123"
+# a=obj.removeTrailingZeros(num)
+# print(a)
   
+
+# 2716. Minimize String Length
+# class Solution:
+#     def minimizedStringLength(self, s):
+#         a=[]
+#         for i in s:
+#             a.append(i)
+#         b=set(a)
+#         print(b)
+#         return len(b)
+
+# obj=Solution()
+# s = "aaabc"
+# # s = "cbbd"
+# # s = "dddaaa"
+# a=obj.minimizedStringLength(s)
+# print(a)
+
+
+
+# 2903. Find Indices With Index and Value Difference I
+
+# You are given a 0-indexed integer array nums having length n, an integer indexDifference, and an integer valueDifference.
+
+# Your task is to find two indices i and j, both in the range [0, n - 1], that satisfy the following conditions:
+
+# abs(i - j) >= indexDifference, and
+# abs(nums[i] - nums[j]) >= valueDifference
+# Return an integer array answer, where answer = [i, j] if there are two such indices, and answer = [-1, -1] otherwise. If there are multiple choices for the two indices, return any of them.
+
+# Note: i and j may be equal.
+
+
+# Example 1:
+
+# Input: nums = [5,1,4,1], indexDifference = 2, valueDifference = 4
+# Output: [0,3]
+# Explanation: In this example, i = 0 and j = 3 can be selected.
+# abs(0 - 3) >= 2 and abs(nums[0] - nums[3]) >= 4.
+# Hence, a valid answer is [0,3].
+# [3,0] is also a valid answer.
+# Example 2:
+
+# Input: nums = [2,1], indexDifference = 0, valueDifference = 0
+# Output: [0,0]
+# Explanation: In this example, i = 0 and j = 0 can be selected.
+# abs(0 - 0) >= 0 and abs(nums[0] - nums[0]) >= 0.
+# Hence, a valid answer is [0,0].
+# Other valid answers are [0,1], [1,0], and [1,1].
+# Example 3:
+
+# Input: nums = [1,2,3], indexDifference = 2, valueDifference = 4
+# Output: [-1,-1]
+# Explanation: In this example, it can be shown that it is impossible to find two indices that satisfy both conditions.
+# Hence, [-1,-1] is returned.
+
+class Solution:
+    def findIndices(self, nums, indexDifference, valueDifference ):
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+indexDifference,n):
+                if abs(i-j)>=indexDifference and abs(nums[i]-nums[j])>=valueDifference:
+                    return [i,j]
+               
+        return [-1,-1]
+    
+
+    
+obj=Solution()
+nums = [5,1,4,1]
+indexDifference = 2
+valueDifference = 4
+
+# nums = [2,1]
+# indexDifference = 0
+# valueDifference = 0
+
+# nums = [1,2,3]
+# indexDifference = 2
+# valueDifference = 4
+a=obj.findIndices(nums,valueDifference,indexDifference)
+print(a)
+        
+
