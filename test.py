fa6b660b7525ca970d75e85858a3dc3c43686c15
“shamnad” <hamzashamnad123@gmail.com>
2024-02-08 16:11:43 +0530

    lc 2859

--- test.py
+++ test.py
@@ -1059,34 +1059,89 @@
 # Output: 7
 # Explanation: Points intersecting at least one car are 1, 2, 3, 5, 6, 7, 8. There are a total of 7 points, therefore the answer would be 7.
 
-class Solution:
-    def numberOfPoints(self, nums):
-        newset=set()
-        for i in nums:
-            print(i,'aaaa')
-            for j in range(i[0],i[1]+1):
-                print(i[0],'bbbbbb')
-                print(i[1],'cccccc')
-                print(i[1]+1,'ddddddd')
-                print(j,'eeeeeeeeeeeeeeee')
+# class Solution:
+#     def numberOfPoints(self, nums):
+#         newset=set()
+#         for i in nums:
+#             print(i,'aaaa')
+#             for j in range(i[0],i[1]+1):
+#                 print(i[0],'bbbbbb')
+#                 print(i[1],'cccccc')
+#                 print(i[1]+1,'ddddddd')
+#                 print(j,'eeeeeeeeeeeeeeee')
                 
-                newset.add(j)
-                print(newset,'kkkkkkkkkkk')
-        return len(newset)
+#                 newset.add(j)
+#                 print(newset,'kkkkkkkkkkk')
+#         return len(newset)
 
-obj=Solution()
-nums = [[3,6],[1,5],[4,7]]
-# nums = [[1,3],[5,8]]
-a=obj.numberOfPoints(nums)
-
-
+# obj=Solution()
+# nums = [[3,6],[1,5],[4,7]]
+# # nums = [[1,3],[5,8]]
+# a=obj.numberOfPoints(nums)
 
 
+# t=3
+# x=bin(t)
+# print(x)
 
 
 
+# 2859. Sum of Values at Indices With K Set Bits
+# You are given a 0-indexed integer array nums and an integer k.
+
+# Return an integer that denotes the sum of elements in nums whose corresponding indices have exactly k set bits in their binary representation.
+
+# The set bits in an integer are the 1's present when it is written in binary.
+
+# For example, the binary representation of 21 is 10101, which has 3 set bits.
+ 
+# Example 1:
+
+# Input: nums = [5,10,1,5,2], k = 1
+# Output: 13
+# Explanation: The binary representation of the indices are: 
+# 0 = 0002
+# 1 = 0012
+# 2 = 0102
+# 3 = 0112
+# 4 = 1002 
+# Indices 1, 2, and 4 have k = 1 set bits in their binary representation.
+# Hence, the answer is nums[1] + nums[2] + nums[4] = 13.
+# Example 2:
+
+# Input: nums = [4,3,2,1], k = 2
+# Output: 1
+# Explanation: The binary representation of the indices are:
+# 0 = 002
+# 1 = 012
+# 2 = 102
+# 3 = 112
+# Only index 3 has k = 2 set bits in its binary representation.
+# Hence, the answer is nums[3] = 1.
 
 
+# class Solution:
+#     def sumIndicesWithKSetBits(self, nums,k):
+#         p=[]
+#         for i in nums:
+#                 z=bin(i)
+#                 c=z.replace('0b','')
+#                 l=c.count('1')
+                
+#                 if k==l:
+#                         p.append(nums[i])
+#                 s=sum(p)
+                        
+#         return s
+            
+# obj=Solution()
+# # nums = [4,3,2,1]
+# # k = 2
+
+# nums = [5,10,1,5,2]
+# k = 1
+# a=obj.sumIndicesWithKSetBits(nums,k)
+# print(a)
 
 
 
