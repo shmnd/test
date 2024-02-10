44b49ed8870d2ccce193f8c8fd2e1cc62e310337
“shamnad” <hamzashamnad123@gmail.com>
2024-02-10 15:13:34 +0530

    lc 2913

--- test.py
+++ test.py
@@ -1323,27 +1323,86 @@
 # 1 <= nums[i] <= 50
 
 # LC 2908
+# class Solution:
+#     def minimumSum(self, nums):
+#         s=0
+#         arr=[]
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 for k in range(j+1,len(nums)):
+#                     if i<j<k :
+#                         if nums[i]<nums[j] and nums[k]<nums[j]:
+#                             s=nums[i]+nums[j]+nums[k]
+#                             arr.append(s)
+
+#         if arr:
+#             print(arr)
+#             return min(arr)
+
+#         else:
+#             return -1
+# obj=Solution()
+# # nums = [8,6,1,5,3]
+# nums = [5,4,8,7,10,2]
+# # nums = [6,5,4,3,4,5]
+# a=obj.minimumSum(nums)
+# print(a)
+
+# 2913. Subarrays Distinct Element Sum of Squares I
+# You are given a 0-indexed integer array nums.
+
+# The distinct count of a subarray of nums is defined as:
+
+# Let nums[i..j] be a subarray of nums consisting of all the indices from i to j such that 0 <= i <= j < nums.length. Then the number of distinct values in nums[i..j] is called the distinct count of nums[i..j].
+# Return the sum of the squares of distinct counts of all subarrays of nums.
+
+# A subarray is a contiguous non-empty sequence of elements within an array.
+
+# Example 1:
+# Input: nums = [1,2,1]
+# Output: 15
+# Explanation: Six possible subarrays are:
+# [1]: 1 distinct value
+# [2]: 1 distinct value
+# [1]: 1 distinct value
+# [1,2]: 2 distinct values
+# [2,1]: 2 distinct values
+# [1,2,1]: 2 distinct values
+# The sum of the squares of the distinct counts in all subarrays is equal to 12 + 12 + 12 + 22 + 22 + 22 = 15.
+
+# Example 2:
+# Input: nums = [1,1]
+# Output: 3
+# Explanation: Three possible subarrays are:
+# [1]: 1 distinct value
+# [1]: 1 distinct value
+# [1,1]: 1 distinct value
+# The sum of the squares of the distinct counts in all subarrays is equal to 12 + 12 + 12 = 3.
+ 
+# Constraints:
+# 1 <= nums.length <= 100
+# 1 <= nums[i] <= 100
+
+
+# lc 2913
 class Solution:
-    def minimumSum(self, nums):
-        s=0
-        arr=[]
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                for k in range(j+1,len(nums)):
-                    if i<j<k :
-                        if nums[i]<nums[j] and nums[k]<nums[j]:
-                            s=nums[i]+nums[j]+nums[k]
-                            arr.append(s)
+    def sumCounts(self, nums):
+      res=0
+      n=len(nums)
 
-        if arr:
-            print(arr)
-            return min(arr)
+      for i in range(n):
+        print(i,'aaaaaa')
+        distinct=set()
+        for j in range(i,n):
+           print(nums[j],'bbbbbbbbbb')
+           distinct.add(nums[j])
+           res+= len(distinct)**2
+           print(len(distinct),'ccccccc')
+           print(res,'dddddddd')
+            
 
-        else:
-            return -1
 obj=Solution()
-# nums = [8,6,1,5,3]
-nums = [5,4,8,7,10,2]
-# nums = [6,5,4,3,4,5]
-a=obj.minimumSum(nums)
+# nums = [1,1]
+nums = [1,2,1]
+a=obj.sumCounts(nums)
 print(a)
