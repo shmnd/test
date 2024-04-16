8116497b71b8aba44c91a91fa226adac15ead02e
“shamnad” <hamzashamnad123@gmail.com>
2024-04-16 09:00:56 +0530

    lc 350

--- test.py
+++ test.py
@@ -2141,14 +2141,47 @@
 
 # lc 334
 
-class Solution:
-    def reverstring(self,s):
-        s.reverse()
-        return s
+# class Solution:
+#     def reverstring(self,s):
+#         s.reverse()
+#         return s
 
-obj=Solution()
-s = ["h","e","l","l","o"]
-a=obj.reverstring(s)
-print(a)
+# obj=Solution()
+# s = ["h","e","l","l","o"]
+# a=obj.reverstring(s)
+# print(a)
     
         
+# lc 350
+
+# Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+# Example 1:
+
+# Input: nums1 = [1,2,2,1], nums2 = [2,2]
+# Output: [2,2]
+# Example 2:
+
+# Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+# Output: [4,9]
+# Explanation: [9,4] is also accepted.
+
+class Solution:
+    def intersect(self, nums1, nums2) :
+        count={}
+        for i in nums1:
+            if i in count:
+                count[i]+=1
+            else:
+                count[i]=1
+        
+        res=[]
+        for i in nums2:
+            if i in count and count[i]>0:
+                res.append(i)
+                count[i]-=1
+        return res
+obj=Solution()
+nums1 = [1,2,2,1]
+nums2 = [2,2]
+a=obj.intersect(nums1,nums2)
+print(a)
