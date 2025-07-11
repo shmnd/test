dccff737ff8276420c907cfb1ecfab393fac3594
Shamnad <hamzashamnad123@gmail.com>
2025-07-11 20:52:38 +0530

    lc 75

--- test.py
+++ test.py
@@ -3768,24 +3768,57 @@ This ensures that the data has not been modified but does not hide the content.
 
 #  lc 121
 
+# class Solution:
+#     def maxProfit(self, prices):
+#         buyPrice = prices[0]
+#         profit = 0
+
+#         for price in prices:
+#             if buyPrice > price:
+#                 buyPrice = price
+#             profit = max(profit, price - buyPrice)
+#         return profit
+
+# obj = Solution()
+# prices = [7,1,5,3,6,4]
+# a= obj.maxProfit(prices)
+# print(a)
+
+
+# lc 75
+
+'''Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.'''
+
+# Input: nums = [2,0,2,1,1,0]
+# Output: [0,0,1,1,2,2]
+
 class Solution:
-    def maxProfit(self, prices):
-        buyPrice = prices[0]
-        profit = 0
+    def sortColors(self, nums):
+        count =[0,0,0]
+        for i in nums:
+            count[i] += 1
 
-        for price in prices:
-            if buyPrice > price:
-                buyPrice = price
-            profit = max(profit, price - buyPrice)
-        return profit
-        
+        index = 0
+        for i in range(3):
+            for j in range(count[i]):
+                nums[index] = i
+                index += 1 
+        return nums
+                
 
-
+        """
+        Do not return anything, modify nums in-place instead.
+        """
 
 obj = Solution()
-prices = [7,1,5,3,6,4]
-a= obj.maxProfit(prices)
+nums = [2,0,2,1,1,0]
+a = obj.sortColors(nums)
 print(a)
+        
 
 
 
