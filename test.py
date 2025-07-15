f6e35667482006fd53e7a0d8b55d32a1080549fa
Shamnad <hamzashamnad123@gmail.com>
2025-07-15 13:52:42 +0530

    lc 238 or 3136

--- test.py
+++ test.py
@@ -3834,21 +3834,102 @@ Output: 5
 Explanation: The last word is "World" with length 5.'''
 
 
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
 
-        words = s.split()
-        len_last_word = len(words[-1])
-        print(len_last_word)
+#         words = s.split()
+#         len_last_word = len(words[-1])
+#         print(len_last_word)
 
 
-        return len_last_word
+#         return len_last_word
     
 
-obj = Solution()
-s = "   fly me   to   the moon  "
-# s = "luffy is still joyboy"
-# s = "Hello World"
-a = obj.lengthOfLastWord(s)
+# obj = Solution()
+# s = "   fly me   to   the moon  "
+# # s = "luffy is still joyboy"
+# # s = "Hello World"
+# a = obj.lengthOfLastWord(s)
+# print(a)
+
+
+# lc 238 or 3136. Valid Word
+
+'''A word is considered valid if:
+
+It contains a minimum of 3 characters.
+It contains only digits (0-9), and English letters (uppercase and lowercase).
+It includes at least one vowel.
+It includes at least one consonant.
+You are given a string word.
+
+Return true if word is valid, otherwise, return false.
+
+Notes:
+
+'a', 'e', 'i', 'o', 'u', and their uppercases are vowels.
+A consonant is an English letter that is not a vowel.'''
+ 
+
+# Example 1:
+
+# Input: word = "234Adas"
+
+# Output: true
+
+# Explanation:
+
+# This word satisfies the conditions.
+
+# Example 2:
+
+# Input: word = "b3"
+
+# Output: false
+
+# Explanation:
+
+# The length of this word is fewer than 3, and does not have a vowel.
+
+# Example 3:
+
+# Input: word = "a3$e"
+
+# Output: false
+
+# Explanation:
+
+# This word contains a '$' character and does not have a consonant.
+
+
+class CorretWord:
+        def FindCorrectWord(self,word):
+
+                if len(word) < 3:
+                        return False
+                
+                c=0
+                v=0
+                vowel_set = "aeiouAEIOU"
+
+                for i in word:
+                        if i.isalpha():
+                                if i in vowel_set:
+                                
+                                        v += 1
+                                else:
+                                        c += 1
+                                
+                        elif not i.isdigit():
+                                return False
+                        
+                return v >=1 and c >=1
+
+obj = CorretWord()
+# word = "234Adas"
+# word = "a3$e"
+# word = "b3"
+word = "bbbbbbb"
+
+a = obj.FindCorrectWord(word)
 print(a)
-              
