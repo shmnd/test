769e17cba4401acb38a04a51aab934c1cc20e38c
Shamnad <hamzashamnad123@gmail.com>
2025-07-16 12:06:31 +0530

    lc 27

--- test.py
+++ test.py
@@ -3902,34 +3902,99 @@ A consonant is an English letter that is not a vowel.'''
 # This word contains a '$' character and does not have a consonant.
 
 
-class CorretWord:
-        def FindCorrectWord(self,word):
+# class CorretWord:
+#         def FindCorrectWord(self,word):
 
-                if len(word) < 3:
-                        return False
+#                 if len(word) < 3:
+#                         return False
                 
-                c=0
-                v=0
-                vowel_set = "aeiouAEIOU"
+#                 c=0
+#                 v=0
+#                 vowel_set = "aeiouAEIOU"
 
-                for i in word:
-                        if i.isalpha():
-                                if i in vowel_set:
+#                 for i in word:
+#                         if i.isalpha():
+#                                 if i in vowel_set:
                                 
-                                        v += 1
-                                else:
-                                        c += 1
+#                                         v += 1
+#                                 else:
+#                                         c += 1
                                 
-                        elif not i.isdigit():
-                                return False
+#                         elif not i.isdigit():
+#                                 return False
                         
-                return v >=1 and c >=1
+#                 return v >=1 and c >=1
 
-obj = CorretWord()
-# word = "234Adas"
-# word = "a3$e"
-# word = "b3"
-word = "bbbbbbb"
+# obj = CorretWord()
+# # word = "234Adas"
+# # word = "a3$e"
+# # word = "b3"
+# word = "bbbbbbb"
 
-a = obj.FindCorrectWord(word)
+# a = obj.FindCorrectWord(word)
+# print(a)
+
+
+# lc 27. Remove Element
+
+
+'''Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int val = ...; // Value to remove
+int[] expectedNums = [...]; // The expected answer with correct length.
+                            // It is sorted with no values equaling val.
+
+int k = removeElement(nums, val); // Calls your implementation
+
+assert k == expectedNums.length;
+sort(nums, 0, k); // Sort the first k elements of nums
+for (int i = 0; i < actualLength; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.'''
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,2,2,3], val = 3
+# Output: 2, nums = [2,2,_,_]
+# Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+# Example 2:
+
+# Input: nums = [0,1,2,2,3,0,4,2], val = 2
+# Output: 5, nums = [0,1,4,0,3,_,_,_]
+# Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+# Note that the five elements can be returned in any order.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+
+class Solution:
+    def removeElement(self, nums,val) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        
+        return k
+
+
+obj = Solution()
+nums = [3,2,2,3]
+val = 3
+a = obj.removeElement(nums,val)
 print(a)
+
+        
