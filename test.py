7229c1a815216bdfdcc56f180122d2f8ac077924
Shamnad <hamzashamnad123@gmail.com>
2025-07-19 17:02:56 +0530

    lc 1394

--- test.py
+++ test.py
@@ -4002,24 +4002,82 @@ If all assertions pass, then your solution will be accepted.'''
 # sort and find unique of a list
 
 
-lis = [ 1, 4, 2, 4, 3, 7, 1 ]
-uni = []
+# lis = [ 1, 4, 2, 4, 3, 7, 1 ]
+# uni = []
 
 # find unique numbers
 
-for i in lis:
-    if i not in uni:
-        uni.append(i)
+# for i in lis:
+#     if i not in uni:
+#         uni.append(i)
 
-print(f'{uni},unique numbers only')
+# print(f'{uni},unique numbers only')
 
-# sorting unique number
+# # sorting unique number
 
-for i in range(len(uni)):
-    for j in range(len(uni)-i-1):
-        if uni[j] > uni[j+1]:
-            uni[j],uni[j+1] = uni[j+1],uni[j]
+# for i in range(len(uni)):
+#     for j in range(len(uni)-i-1):
+#         if uni[j] > uni[j+1]:
+#             uni[j],uni[j+1] = uni[j+1],uni[j]
 
-print(f'{uni},sorted unique number')
+# print(f'{uni},sorted unique number')
 
+
+# lc 1394
+
+'''1394. Find Lucky Integer in an Array
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+
+Return the largest lucky integer in the array. If there is no lucky integer return -1.'''
+
+ 
+
+# Example 1:
+
+# Input: arr = [2,2,3,4]
+# Output: 2
+# Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+# Example 2:
+
+# Input: arr = [1,2,2,3,3,3]
+# Output: 3
+# Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+# Example 3:
+
+# Input: arr = [2,2,2,3,3]
+# Output: -1
+# Explanation: There are no lucky numbers in the array.
+ 
+
+'''Constraints:
+
+1 <= arr.length <= 500
+1 <= arr[i] <= 500
+'''
+
+
+class Solution:
+    def findLucky(self, arr):
+
+        count = {}
+        lucky = -1
+        for i in arr:
+            count[i] = count.get(i,0) +1
+        
+        for key,val in count.items():
+            if key == val:
+                if key > lucky:
+                    lucky = key
+        return lucky
+
+obj = Solution()
+# arr = [2,2,3,4]
+arr = [1,2,2,3,3,3]
+a = obj.findLucky(arr)
+print(a)
 
