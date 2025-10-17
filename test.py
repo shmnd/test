db4b3abdf0e8891af5ebf20840ff967f2cb7ba26
Shamnad <hamzashamnad123@gmail.com>
2025-10-17 20:44:45 +0530

    lc 1672

--- test.py
+++ test.py
@@ -4339,22 +4339,78 @@ Constraints:
 1 <= numExchange <= 100'''
 
 
-class Solution:
-    def maxBottleDrunk(self,numBottle,numExchange):
-        drunk_bottle = numBottle
-        empty_bottle = numBottle
+# class Solution:
+#     def maxBottleDrunk(self,numBottle,numExchange):
+#         drunk_bottle = numBottle
+#         empty_bottle = numBottle
 
-        while empty_bottle >= numExchange:
-            empty_bottle -= numExchange
-            drunk_bottle +=1
-            numExchange +=1
-        #     print(numExchange)
-            empty_bottle +=1
+#         while empty_bottle >= numExchange:
+#             empty_bottle -= numExchange
+#             drunk_bottle +=1
+#             numExchange +=1
+#         #     print(numExchange)
+#             empty_bottle +=1
 
-        return drunk_bottle
+#         return drunk_bottle
     
 
 
-obj = Solution()
-# print(obj.maxBottleDrunk(13,6))
-print(obj.maxBottleDrunk(10,3))
+# obj = Solution()
+# # print(obj.maxBottleDrunk(13,6))
+# print(obj.maxBottleDrunk(10,3))
+
+
+# lc 1672
+
+"""1672. Richest Customer Wealth
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+ 
+
+Example 1:
+
+Input: accounts = [[1,2,3],[3,2,1]]
+Output: 6
+Explanation:
+1st customer has wealth = 1 + 2 + 3 = 6
+2nd customer has wealth = 3 + 2 + 1 = 6
+Both customers are considered the richest with a wealth of 6 each, so return 6.
+Example 2:
+
+Input: accounts = [[1,5],[7,3],[3,5]]
+Output: 10
+Explanation: 
+1st customer has wealth = 6
+2nd customer has wealth = 10 
+3rd customer has wealth = 8
+The 2nd customer is the richest with a wealth of 10.
+Example 3:
+
+Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
+Output: 17"""
+class Solution:
+    
+
+    def richestCustomer(self,acc):
+        arr = []
+        val = 0
+        for i in acc:
+            val = 0
+            for j in range(len(i)):
+                val += i[j]
+                if j+1 == len(i):
+                        arr.append(val)
+                
+        return max(arr)
+
+obj =Solution()
+print(obj.richestCustomer([[1,2,3],[3,2,1]]))
+# print(obj.richestCustomer([[1,5],[7,3],[3,5]]))
